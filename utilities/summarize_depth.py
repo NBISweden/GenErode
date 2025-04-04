ac15a0954696973e95ccda31105a17ab481861e9
verku <verena.kutschera@scilifelab.se>
2025-04-04 16:31:59 +0200

    Add documentation and option to provide the directory on the command line

--- utilities/summarize_depth.py
+++ utilities/summarize_depth.py
@@ -1,10 +1,27 @@
+#!/usr/bin/python3
+
+"""
+Author: Julius Falck
+
+Script to concatenate all depth statistics files from the bam_processing step into one file,
+including mean genome-wide depth, minimum depth threshold and maximum depth threshold as 
+calculated in GenErode for depth filtering.
+
+Usage:
+Move into the directory "GenErode/" and start the script from the command line with 
+the following command, replacing "[dataset]" with modern or historical and "[reference_name]" 
+with the name of the reference genome used for mapping:
+
+python summarize_depth.py results/[dataset]/mapping/[reference_name]/stats/bams_indels_realigned/
+
+e.g. 
+python summarize_depth.py results/modern/mapping/sumatran_rhinoceros/stats/bams_indels_realigned/
+"""
+
 import os
+import sys
 
-# Author: Julius Falck
-# change directory to the bams_indels_realigned folder in the results
-
-directory= ""
-
+directory = sys.argv[1]
 dir_list = os.listdir(directory)
 
 depth_table = "depth_table.txt"
